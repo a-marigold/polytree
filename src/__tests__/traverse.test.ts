81390aebeadae0f39e8f770fe00f154ca7e71158
NextKolya <phelixgluy@gmail.com>
2026-03-28 21:38:44 +0600

    update tests for onEnter and onExit

--- src/__tests__/traverse.test.ts
+++ src/__tests__/traverse.test.ts
@@ -1,6 +1,125 @@
 import { describe, it, expect, vi } from 'bun:test';
 
 import { traverse } from 'src/traverse';
+import { SKIP, STOP } from 'src/constants';
+
+import type { NodeLike } from 'src/types';
+
+const testTraverseVisitor = (visitorName: 'onEnter' | 'onExit') => {
+    it(`should mutate AST if \`${visitorName}\` returned an object`, () => {
+        const ast = {
+            type: 'a',
+            b: {
+                type: 'b',
+                c: {
+                    type: 'c',
+                    children: [{ type: 'a' }, { type: 'b' }, { type: 'c' }],
+                },
+            },
+        };
+
+        const astSnapshot = JSON.stringify(ast);
+
+        const visitor = (node: NodeLike) => {
+            if (node.type === 'b') {
+                return { type: 'abc' };
+            }
+        };
+
+        traverse(
+            ast,
+
+            visitorName === 'onEnter' ? visitor : null,
+            visitorName === 'onExit' ? visitor : null,
+        );
+
+        expect(astSnapshot).not.toBe(JSON.stringify(ast));
+
+        expect(ast).toMatchInlineSnapshot(`
+          {
+            "b": {
+              "type": "abc",
+            },
+            "type": "a",
+          }
+        `);
+    });
+
+    it(`should mutate AST if \`${visitorName}\` returned an object`, () => {
+        const ast = {
+            type: 'a',
+
+            b: {
+                type: 'b',
+
+                c: {
+                    type: 'c',
+
+                    children: [{ type: 'a' }, { type: 'b' }, { type: 'c' }],
+                },
+            },
+        };
+
+        const astSnapshot = JSON.stringify(ast);
+
+        const visitor = (node: NodeLike) => {
+            if (node.type === 'b') {
+                return { type: 'abc' };
+            }
+        };
+
+        traverse(
+            ast,
+
+            visitorName === 'onEnter' ? visitor : null,
+            visitorName === 'onExit' ? visitor : null,
+        );
+
+        expect(astSnapshot).not.toBe(JSON.stringify(ast));
+
+        expect(ast).toMatchInlineSnapshot(`
+          {
+            "b": {
+              "type": "abc",
+            },
+            "type": "a",
+          }
+        `);
+    });
+
+    it(`should stop immediatly when \`${visitorName}\` returned \`STOP\``, () => {
+        let visited = '';
+
+        const visitor = (node: NodeLike) => {
+            visited += node.type;
+
+            if (node.type === 'c') {
+                return STOP;
+            }
+        };
+
+        traverse(
+            {
+                type: 'a',
+
+                value: {
+                    type: 'b',
+                    value: {
+                        type: 'd',
+                    },
+                    value2: {
+                        type: 'c',
+                    },
+                },
+            },
+
+            visitorName === 'onEnter' ? visitor : null,
+            visitorName === 'onExit' ? visitor : null,
+        );
+
+        expect(visited).not.toInclude('d');
+    });
+};
 
 describe('traverse', () => {
     it('should visit every property that is an object and has `type` property', () => {
@@ -65,81 +184,7 @@ describe('traverse', () => {
             expect(onEnter.mock.calls.length).toBe(onExit.mock.calls.length);
         });
 
-        it('should mutate AST if `onEnter` returned an object', () => {
-            const ast = {
-                type: 'a',
-                b: {
-                    type: 'b',
-                    c: {
-                        type: 'c',
-                        children: [{ type: 'a' }, { type: 'b' }, { type: 'c' }],
-                    },
-                },
-            };
-
-            const astSnapshot = JSON.stringify(ast);
-
-            traverse(
-                ast,
-                (node) => {
-                    if (node.type === 'b') {
-                        return { type: 'abc' };
-                    }
-                },
-
-                null,
-            );
-
-            expect(astSnapshot).not.toBe(JSON.stringify(ast));
-
-            expect(ast).toMatchInlineSnapshot(`
-          {
-            "b": {
-              "type": "abc",
-            },
-            "type": "a",
-          }
-        `);
-        });
-
-        it('should mutate AST if `onExit` returned an object', () => {
-            const ast = {
-                type: 'a',
-
-                b: {
-                    type: 'b',
-
-                    c: {
-                        type: 'c',
-                        children: [{ type: 'a' }, { type: 'b' }, { type: 'c' }],
-                    },
-                },
-            };
-
-            const astSnapshot = JSON.stringify(ast);
-
-            traverse(
-                ast,
-
-                () => {},
-
-                (nodessssss) => {
-                    if (nodessssss.type === 'b') {
-                        return { type: 'abc' };
-                    }
-                },
-            );
-
-            expect(astSnapshot).not.toBe(JSON.stringify(ast));
-
-            expect(ast).toMatchInlineSnapshot(`
-          {
-            "b": {
-              "type": "abc",
-            },
-            "type": "a",
-          }
-        `);
-        });
+        testTraverseVisitor('onEnter');
+        testTraverseVisitor('onExit');
     });
 });
